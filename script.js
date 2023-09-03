fc102738065ebd93d764553012a2cd2c5255bf71
Luktoria <110913006+Luktoria@users.noreply.github.com>
2023-09-03 10:49:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+/*change facebook icon */
+
+document.getElementById("facebook").addEventListener('mouseover', ()=> {document.getElementById("facebook").src="./images/icon-facebook-white.svg"});
+document.getElementById("facebook").addEventListener('mouseleave', ()=> {document.getElementById("facebook").src="./images/icon-facebook.svg"});
+
+
+/*change instagram icon */
+
+document.getElementById("instagram").addEventListener('mouseover', ()=> {document.getElementById("instagram").src="./images/icon-instagram-white.svg"});
+document.getElementById("instagram").addEventListener('mouseleave', ()=> {document.getElementById("instagram").src="./images/icon-instagram.svg"});
+
+
+/*change twitter icon */
+
+document.getElementById("twitter").addEventListener('mouseover', ()=> {document.getElementById("twitter").src="./images/icon-twitter-white.svg"});
+document.getElementById("twitter").addEventListener('mouseleave', ()=> {document.getElementById("twitter").src="./images/icon-twitter.svg"});
+
+
+
+/*change pinterest icon */
+
+document.getElementById("pinterest").addEventListener('mouseover', ()=> {document.getElementById("pinterest").src="./images/icon-pinterest-white.svg"});
+document.getElementById("pinterest").addEventListener('mouseleave', ()=> {document.getElementById("pinterest").src="./images/icon-pinterest.svg"});
+
+
+/*Make menu at click*/
+
+function makeMenu(){
+    document.getElementById('hambuger-menu').style.display="block";
+    document.getElementsByClassName('heading')[0].style.display="none";
+    };
+    
+
+document.getElementById('hamburger').addEventListener('mouseenter', makeMenu);
+document.getElementById('hambuger-menu').addEventListener("mouseenter", makeMenu);
+
+function hideMenu(){
+    document.getElementById('hambuger-menu').style.display="none";
+    document.getElementsByClassName('heading')[0].style.display="block";
+}
+
+document.getElementById('hamburger').addEventListener('mouseleave', hideMenu);
+document.getElementById('hambuger-menu').addEventListener("mouseleave", hideMenu);
